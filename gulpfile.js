769bb6147564a40a829bd652543fa71c92873cee
Gajus Kuizinas <gk@anuary.com>
2014-11-14 15:01:36 +0200

    Clean up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
-    clean = require('gulp-clean'),
     jshint = require('gulp-jshint'),
-    header = require('gulp-header'),
-    regenerator = require('gulp-regenerator'),
-    jsonfile = require('jsonfile'),
-    pkg = jsonfile.readFileSync('package.json');
+    gitdown = require('./src/gitdown.js');
 
 gulp.task('lint', function () {
     return gulp
@@ -14,32 +10,19 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('clean', ['lint'], function () {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('regenerate', ['lint'], function () {
-    return gulp.src('src/' + pkg.name + '.js')
-        //.pipe(regenerator({includeRuntime: true}))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('version', ['regenerate'], function () {
-    return gulp
-        .src('./dist/' + pkg.name + '.js')
-        .pipe(header('/**\n * @version <%= version %>\n * @link https://github.com/gajus/<%= name %> for the canonical source repository\n * @license https://github.com/gajus/<%= name %>/blob/master/LICENSE BSD 3-Clause\n */\n', {version: pkg.version, name: pkg.name}))
-        .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('test', ['version'], function () {
+gulp.task('test', ['lint'], function () {
     return gulp
         .src('./tests/*.js', {read: false})
         .pipe(mocha());
 });
 
+gulp.task('gitdown', function (cb) {
+
+});
+
 gulp.task('watch', function () {
     gulp.watch(['./src/*', './src/helpers/*', './tests/*'], ['default']);
+    gulp.watch(['./.gitdown/*', './.gitdown/helpers/*'], ['gitdown']);
 });
 
 gulp.task('default', ['test']);
